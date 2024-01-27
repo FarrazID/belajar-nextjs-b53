0ca2ad8d7f39776ff0bf9390a5dddf0071f759bf
FarrazID2 <farraz02@gmail.com>
2024-01-27 13:45:13 +0700

    update API routes - to call external API

--- nextjs-page-route1/pages/api/hello.js
+++ nextjs-page-route1/pages/api/hello.js
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  // res.status(200).json({ name: 'John Doe' })
+//! API Routes -- to create public API
+//? possible to write BACKEND code in NextJS
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+//! API Local -- call External API
+export default async function handler(req, res) {
   try {
-    const response = await(fetch('https://dummyjson.com/users')).json();
+    const response = await (q).json();
     res.status(200).json({ ...response });
 
   } catch (error) {
-    res.status(500).json({ error })
+    // res.status(500).json({ error })
   }
 
 }
